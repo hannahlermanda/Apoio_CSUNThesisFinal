e95b84cb2c7cbacd1b7007ff9a68e68fd2d54d66
hannahlermanda <hannahlermanda@gmail.com>
2024-11-14 04:12:26 -0800

    First commit; Added Redux

---
+++ app/index.tsx
@@ -0,0 +1,18 @@
+import { Text, View } from "react-native";
+
+//1) Set up  Redux
+
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Let's build UBER! :D</Text>
+    </View>
+  );
+}
