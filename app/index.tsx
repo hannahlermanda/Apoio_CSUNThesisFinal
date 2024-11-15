070b0745c0d24b156a25eb8c3dc98d9a65d366d1
hannahlermanda <hannahlermanda@gmail.com>
2024-11-14 17:14:50 -0800

    Tested Basic Styling

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,14 @@
 import { Text, View } from "react-native";
 import { Provider } from "react-redux"
 import { store } from "@/app/store";
+import HomeScreen from "@/app/screens/HomeScreen";
 //1) Set up  Redux
 
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Let's build UBER! :D</Text>
-    </View>
+      <HomeScreen />
     </Provider>
   );
 }
